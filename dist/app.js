66c6cd713e7a803400789c5848ef4cd469ae0990
Lu Yu <yulu.nju@gmail.com>
2022-11-24 21:09:56 -0800

    update babel config
    Signed-off-by: Lu Yu <yulu.nju@gmail.com>

--- dist/app.js
+++ dist/app.js
@@ -5,17 +5,23 @@ Object.defineProperty(exports, "__esModule", {
 });
 exports["default"] = void 0;
 require("./app.css");
+var _jsxRuntime = require("react/jsx-runtime");
 function App() {
-  return /*#__PURE__*/React.createElement("div", {
-    className: "App"
-  }, /*#__PURE__*/React.createElement("header", {
-    className: "App-header"
-  }, /*#__PURE__*/React.createElement("p", null, "Welcome to OpenSearch Playground"), /*#__PURE__*/React.createElement("a", {
-    className: "Playground-link",
-    href: "https://github.com/opensearch-project/dashboards-anywhere",
-    target: "_blank",
-    rel: "noopener noreferrer"
-  }, "Playground github link")));
+  return /*#__PURE__*/(0, _jsxRuntime.jsx)("div", {
+    className: "App",
+    children: /*#__PURE__*/(0, _jsxRuntime.jsxs)("header", {
+      className: "App-header",
+      children: [/*#__PURE__*/(0, _jsxRuntime.jsx)("p", {
+        children: "Welcome to OpenSearch Playground"
+      }), /*#__PURE__*/(0, _jsxRuntime.jsx)("a", {
+        className: "Playground-link",
+        href: "https://github.com/opensearch-project/dashboards-anywhere",
+        target: "_blank",
+        rel: "noopener noreferrer",
+        children: "Playground github link"
+      })]
+    })
+  });
 }
 var _default = App;
 exports["default"] = _default;
