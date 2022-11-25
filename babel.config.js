c097acb01144020562744665febba1ca2c11c255
Lu Yu <yulu.nju@gmail.com>
2022-11-24 16:54:56 -0800

    add babel
    Signed-off-by: Lu Yu <yulu.nju@gmail.com>

---
+++ babel.config.js
@@ -0,0 +1,14 @@
+// babelrc doesn't respect NODE_PATH anymore but using require does.
+// Alternative to install them locally in node_modules
+module.exports = {
+    presets: [
+      require('@babel/preset-env'),
+      require('@babel/preset-react'),
+      require('@babel/preset-typescript'),
+    ],
+    plugins: [
+      require('@babel/plugin-proposal-class-properties'),
+      require('@babel/plugin-proposal-object-rest-spread'),
+      require('@babel/plugin-transform-modules-commonjs'),
+    ],
+  };
