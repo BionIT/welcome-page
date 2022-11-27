7591d5cede7ee0fc9fe3d7f48f92cefa8719c9ab
Lu Yu <yulu.nju@gmail.com>
2022-11-26 17:02:02 -0800

    use eui lib to construct welcome page
    Signed-off-by: Lu Yu <yulu.nju@gmail.com>

--- dist/app.js
+++ dist/app.js
@@ -5,20 +5,39 @@ Object.defineProperty(exports, "__esModule", {
 });
 exports["default"] = void 0;
 require("./app.css");
+var _eui = require("@elastic/eui");
+var _opensearch_mark_default = _interopRequireDefault(require("./opensearch_mark_default.svg"));
+require("@elastic/eui/dist/eui_theme_light.css");
 var _jsxRuntime = require("react/jsx-runtime");
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
 function App() {
-  return /*#__PURE__*/(0, _jsxRuntime.jsx)("div", {
-    className: "App",
-    children: /*#__PURE__*/(0, _jsxRuntime.jsxs)("header", {
-      className: "App-header",
-      children: [/*#__PURE__*/(0, _jsxRuntime.jsx)("p", {
-        children: "Welcome to OpenSearch Playground"
-      }), /*#__PURE__*/(0, _jsxRuntime.jsx)("a", {
-        className: "Playground-link",
+  return /*#__PURE__*/(0, _jsxRuntime.jsx)(_eui.EuiEmptyPrompt, {
+    icon: /*#__PURE__*/(0, _jsxRuntime.jsx)("img", {
+      src: _opensearch_mark_default["default"],
+      alt: "",
+      width: 40
+    }),
+    title: /*#__PURE__*/(0, _jsxRuntime.jsx)("h2", {
+      children: "Welcome"
+    }),
+    body: /*#__PURE__*/(0, _jsxRuntime.jsx)("p", {
+      children: "OpenSearch Playground serves as a live demo environment of OpenSearch and OpenSearch Dashboards where you can explore OpenSearch"
+    }),
+    actions: /*#__PURE__*/(0, _jsxRuntime.jsx)(_eui.EuiButton, {
+      color: "primary",
+      fill: true,
+      children: "Explore"
+    }),
+    footer: /*#__PURE__*/(0, _jsxRuntime.jsxs)(_jsxRuntime.Fragment, {
+      children: [/*#__PURE__*/(0, _jsxRuntime.jsx)(_eui.EuiTitle, {
+        size: "xxs",
+        children: /*#__PURE__*/(0, _jsxRuntime.jsx)("h3", {
+          children: "Want to learn more?"
+        })
+      }), /*#__PURE__*/(0, _jsxRuntime.jsx)(_eui.EuiLink, {
         href: "https://github.com/opensearch-project/dashboards-anywhere",
         target: "_blank",
-        rel: "noopener noreferrer",
-        children: "Playground github link"
+        children: "Checkout the code base"
       })]
     })
   });
--- src/lib/app.js
+++ src/lib/app.js
@@ -1,23 +1,38 @@
 import './app.css';
+import {
+  EuiEmptyPrompt,
+  EuiButton,
+  EuiTitle,
+  EuiLink
+} from '@elastic/eui';
+import illustration from './opensearch_mark_default.svg';
+import '@elastic/eui/dist/eui_theme_light.css';
+
 
 function App() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <p>
-            Welcome to OpenSearch Playground
-          </p>
-          <a
-            className="Playground-link"
-            href="https://github.com/opensearch-project/dashboards-anywhere"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Playground github link
-          </a>
-        </header>
-      </div>
-    );
+      <EuiEmptyPrompt
+    icon={<img src={illustration} alt="" width={40}/>}
+    title={<h2>Welcome</h2>}
+    body={<p>OpenSearch Playground serves as a live demo environment of OpenSearch and OpenSearch Dashboards where you can explore OpenSearch</p>}
+    actions={
+      <EuiButton color="primary" fill>
+        Explore
+      </EuiButton>
+    }
+    footer={
+      <>
+        <EuiTitle size="xxs">
+          <h3>Want to learn more?</h3>
+        </EuiTitle>
+        <EuiLink href="https://github.com/opensearch-project/dashboards-anywhere" target="_blank">
+          Checkout the code base
+        </EuiLink>
+      </>
+    }
+  />
+  );
+  
   }
   
   export default App;
